2acdfe3dcca1cf3fbd687a332eed01be7e21a4f9
oebelus <imaneelfidrissi@hotmail.com>
2024-08-10 22:18:54 +0100

    file hierarchy

--- Language/Interpreter/Lexer/Token.cs
+++ Language/Interpreter/Lexer/Token.cs
@@ -1,3 +1,16 @@
+class Token(TokenType type, string lexeme, object literal, int line)
+{
+    public TokenType Type = type;
+    public string Lexeme = lexeme;
+    public object Literal = literal ?? "null";
+    public int Line = line;
+
+    public static void TokenLogger(Token token)
+    {
+        Console.WriteLine($"Type: {token.Type}, Lexeme: {token.Lexeme}, Literal: {token.Literal}, Line: {token.Line}");
+    }
+}
+
 enum TokenType
 {
     // Punctuators.
